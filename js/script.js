cf31194f761afd623fb98589f658b5f61c049765
ryanzin10 <kleber.ryan4c@gmail.com>
2023-03-28 21:54:57 -0300

    Adicionando cadastro de clientes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
+  const burger = document.querySelector('.burger', '.link');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
 
@@ -12,7 +12,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        link.style.animation = 'navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s';
       }
     });
 
@@ -22,3 +22,6 @@ const navSlide = () => {
 };
 
 navSlide();
+
+
+
